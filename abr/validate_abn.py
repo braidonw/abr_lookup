e281b4964e82d1d6cc78aa5d6b6f7452db2be4f8
Braidon Whatley <braidonw@gmail.com>
2021-03-04 14:04:50 +1100

    comments change

--- abr/validate_abn.py
+++ abr/validate_abn.py
@@ -1,5 +1,8 @@
 def validate_abn(abn) -> bool:
+
+    # Provided by the ABR
     weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
     try:
         abn = list(int(i) for i in str(abn).replace(" ", ""))
     except ValueError:
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from abr.fetcher import Fetcher
 
 if __name__ == "__main__":
     t = Fetcher()
-    items = ["97785358970", "Secus Digital"]
+    items = ["97785358970", "Secus Digital", "Woolworths"]
     records = t.parallel_search(items)
     print(records)
